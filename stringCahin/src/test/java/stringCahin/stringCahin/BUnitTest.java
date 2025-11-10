7b3c850dfe9ab75d36a1a47ed55b8c4675a96b0f
Bar-Ohana <Barohana5896@gmail.com>
2025-11-10 16:20:41 +0200

    Second

--- stringCahin/src/test/java/stringCahin/stringCahin/BUnitTest.java
+++ stringCahin/src/test/java/stringCahin/stringCahin/BUnitTest.java
@@ -1,4 +1,19 @@
 package stringCahin.stringCahin;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class BUnitTest {
+    @Test
+    public void testCWithStub() {
+        // יוצרים סטאב של D
+        CStub stub = new CStub();
+        // יוצרים את C עם הסטאב
+        B b = new B(stub);
+
+        // קוראים ל־C — לא לסטאב!
+        String result = b.process("B");
+
+        // מאמתים את הפלט
+        assertEquals("BE", result);
+    }
 }
